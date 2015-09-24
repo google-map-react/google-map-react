0006f2bfbec2b1a591585a928778217c7df031bc
cybice <istarkov@gmail.com>
2015-09-24 21:07:58 +0300

    Remove umd build from sources

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,4 +10,4 @@ $babel ./src -d $build_dir --ignore "__tests__" --loose all
 NODE_ENV=production $webpack src/index.js $build_dir/umd/GoogleMapReact.js
 NODE_ENV=production $webpack -p src/index.js $build_dir/umd/GoogleMapReact.min.js
 
-echo "gzipped, the global build is `gzip -c $build_dir/umd/GoogleMapReact.min.js | wc -c | sed -e 's/^[[:space:]]*//'` bytes"
+#echo "gzipped, the global build is `gzip -c $build_dir/umd/GoogleMapReact.min.js | wc -c | sed -e 's/^[[:space:]]*//'` bytes"
