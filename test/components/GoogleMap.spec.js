6c1601228cb1f9807fe349b68563efd63f95abc1
cybice <istarkov@gmail.com>
2015-10-04 00:30:20 +0300

    Add tests for defaultCenter and defaultZoom

--- test/components/GoogleMap.spec.js
+++ test/components/GoogleMap.spec.js
@@ -104,6 +104,54 @@ describe('Components', () => {
     expect(marker.parentNode.style.top).toEqual('-12.62811732746195px');
   });
 
+  it('Should accept defaultCenter and defaultZoom props', () => {
+    const mapMarkerClassName = 'mapMarkerClassName';
+
+    class MapMarker extends Component { // eslint-disable-line react/no-multi-comp
+      render() {
+        return (
+          <div className={mapMarkerClassName}>Marker</div>
+        );
+      }
+    }
+
+    class MapHolder extends Component { // eslint-disable-line react/no-multi-comp
+      static propTypes = {
+        center: PropTypes.array,
+        zoom: PropTypes.number,
+        greatPlaceCoords: PropTypes.any,
+      };
+
+      static defaultProps = {
+        center: {lat: 59.938043, lng: 30.337157},
+        zoom: 9,
+      };
+
+      constructor(props) {
+        super(props);
+      }
+
+      render() {
+        return (
+           <GoogleMap
+            defaultCenter={this.props.center}
+            defaultZoom={this.props.zoom}
+          >
+            <MapMarker lat={59.955413} lng={30.337844} />
+          </GoogleMap>
+        );
+      }
+    }
+
+    const mapHolder = TestUtils.renderIntoDocument(
+      <MapHolder />
+    );
+
+    const marker = TestUtils.findRenderedDOMComponentWithClass(mapHolder, 'mapMarkerClassName');
+    expect(marker.parentNode.style.left).toEqual('0.250129066669615px');
+    expect(marker.parentNode.style.top).toEqual('-12.62811732746195px');
+  });
+
 
   it('Should call custom loader', () => {
     const API_KEY = 'API_KEY';
