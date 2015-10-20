a86076ad9ea00ec6ee840c3d60bd156b9eac9564
martingg88 <mgg8686@gmail.com>
2015-10-20 13:26:48 +0800

    Update google_map_loader.js
    support map with multiple languages.

--- src/utils/loaders/google_map_loader.js
+++ src/utils/loaders/google_map_loader.js
@@ -3,7 +3,7 @@ let $script_ = null;
 let _loadPromise;
 
 // TODO add libraries language and other map options
-export default function googleMapLoader(apiKey) {
+export default function googleMapLoader(apiKey, language) {
   if (!$script_) {
     $script_ = require('scriptjs');
   }
@@ -32,10 +32,11 @@ export default function googleMapLoader(apiKey) {
       resolve(window.google.maps);
     };
 
+    const languageString = language ? '&language=' + language : '';
     const apiKeyString = apiKey ? `&key=${apiKey}` : '';
 
     $script_(
-      `https://maps.googleapis.com/maps/api/js?callback=_$_google_map_initialize_$_${apiKeyString}`,
+      `https://maps.googleapis.com/maps/api/js?callback=_$_google_map_initialize_$_${languageString}${apiKeyString}`,
       () => {
         if (typeof window.google === 'undefined') {
           reject(new Error('google map initialization error (not loaded)'));
