9bd44832bbf5714fe235e814f96da588a700c5eb
Ivan <iqsk81ad@gmail.com>
2016-10-26 14:52:09 +0300

    #add layerTypes props handler, and GMapLayers view

--- develop/Layout.js
+++ develop/Layout.js
@@ -12,6 +12,7 @@ export class Layout extends Component { // eslint-disable-line
         <header className={header}>
           <div className={links}>
             <Link to="/">Multi Markers</Link>
+            <Link to="/layers">With layers</Link>
             <Link to="/hoverunoptim">Hover unoptim</Link>
             <Link to="/hoveroptim">Hover optim</Link>
             <Link to="/resizable">Resizable Map</Link>
--- develop/Main.js
+++ develop/Main.js
@@ -4,6 +4,7 @@ import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import Layout from './Layout';
 import GMap from './GMap';
+import GMapLayers from './GMapLayers';
 import GMapOptim from './GMapOptim';
 import GMapResizable from './GMapResizable';
 
@@ -16,6 +17,7 @@ render(
   <Router history={browserHistory}>
     <Route path="/" component={Layout}>
       <Route markersCount={50} path="hoverunoptim" component={GMap} />
+      <Route markersCount={50} path="layers" component={GMapLayers} />
       <Route markersCount={50} path="hoveroptim" component={GMapOptim} />
       <Route markersCount={20} path="resizable" component={GMapResizable} />
       <IndexRoute markersCount={20} component={GMap} />
