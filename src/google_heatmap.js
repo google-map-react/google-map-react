85ebf8a257f458216fe53df52be1ea26857e8939
ZAKdev <zainahmedkhan@gmail.com>
2017-09-10 16:45:39 +0400

    resolve linting

--- src/google_heatmap.js
+++ src/google_heatmap.js
@@ -1,7 +1,7 @@
 import fp from 'lodash/fp';
 
-export const generateHeatmap = (instance, { positions }) => {
-  return new instance.visualization.HeatmapLayer({
+export const generateHeatmap = (instance, { positions }) =>
+  new instance.visualization.HeatmapLayer({
     data: fp.reduce(
       (acc, { lat, lng }) => {
         acc.push({
@@ -13,11 +13,9 @@ export const generateHeatmap = (instance, { positions }) => {
       positions
     ),
   });
-};
 
-export const optionsHeatmap = (instance, { options }) => {
-  return fp.map(
+export const optionsHeatmap = (instance, { options }) =>
+  fp.map(
     option => instance.set(option, options[option]),
     Object.keys(options || {})
   );
-};
