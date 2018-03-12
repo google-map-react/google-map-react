12a1d4e38085bd44a0c68ee30a5f02745a23dd65
Michael Diego <itsmichaeldiego@gmail.com>
2018-03-12 15:13:37 -0700

    Use private createHelper from recompose as its not exposed

--- develop/utils/withStateSelector.js
+++ develop/utils/withStateSelector.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import { createHelper } from 'recompose';
+import createHelper from 'recompose/createHelper';
 import createEagerFactory from './createEagerFactory';
 
 const withStateSelector = (stateName, stateUpdaterName, selectorFactory) =>
