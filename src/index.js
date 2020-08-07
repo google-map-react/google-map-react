52628215614691ca61a1a250e1e4aefcac7fc0e1
Karl Danninger <karl@danninger.co>
2020-08-06 21:45:09 -0400

    revert to export *

--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
 import GoogleMap from './google_map';
 
-import {
-  fitBounds,
-  meters2ScreenPixels,
-  tile2LatLng,
-  latLng2Tile,
-  getTilesIds,
-} from './lib';
-
-export {
-  fitBounds,
-  meters2ScreenPixels,
-  tile2LatLng,
-  latLng2Tile,
-  getTilesIds,
-};
+export * from './lib';
 
 export default GoogleMap;
