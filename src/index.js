ea5fda3c343af2ccf10454895d12ab11cf93398a
Karl Danninger <karl@danninger.co>
2020-08-06 22:12:49 -0400

    named exports for utilities

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import GoogleMap from './google_map';
 
-export * from './lib';
+export {
+  fitBounds,
+  meters2ScreenPixels,
+  tile2LatLng,
+  latLng2Tile,
+  getTilesIds,
+} from './lib';
 
 export default GoogleMap;
