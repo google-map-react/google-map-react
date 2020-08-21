c688d656a284770071ac9babdde154b2c62aa2ac
Brent Larson <brent.larson@tivityhealth.com>
2020-08-21 16:02:06 -0700

    Manually import/export lib named exports
    - Hopefully this fixes the SSR issue

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import GoogleMap from './google_map';
+import {
+  convertNeSwToNwSe,
+  convertNwSeToNeSw,
+  fitBounds,
+  meters2ScreenPixels,
+  tile2LatLng,
+  latLng2Tile,
+  getTilesIds,
+} from './lib';
 
-export * from './lib';
+export {
+  convertNeSwToNwSe,
+  convertNwSeToNeSw,
+  fitBounds,
+  meters2ScreenPixels,
+  tile2LatLng,
+  latLng2Tile,
+  getTilesIds,
+};
 export default GoogleMap;
