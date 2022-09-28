f4be57377b7ac7913bc4a720a0294b4838698ba8
Alex Bordin <alex.bordin@filoblu.com>
2022-09-28 11:10:51 +0200

    Init markersDispatcher_ on componentDidMount, fixes #1110 #1116 #1145 #1113

--- example/src/App.test.js
+++ example/src/App.test.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 import { createRoot } from 'react-dom/client';
 
-
 it('renders without crashing', () => {
-  const div = document.createElement('div')
+  const div = document.createElement('div');
   const root = createRoot(div);
-  root.render(<App />)
-  ReactDOM.unmountComponentAtNode(div)
-})
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+  ReactDOM.unmountComponentAtNode(div);
+});
